6b30d37bf1757ac2a7aeb87fa30bbb4651183d20
Ddloyd <quocanh25022001@gmail.com>
2022-12-30 14:06:45 +0700

    first commit

--- bai_tap/ss8/nhap_2so.js
+++
@@ -1,9 +0,0 @@
-function checkSo() {
-    let a = parseInt(document.getElementById("a").value);
-    let b = parseInt(document.getElementById("b").value);
-    if ((a % b) === 0) {
-        alert("a chia hết cho b");
-    } else {
-        alert("a không chia hết cho b");
-    }
-}
